e23ff81407b18b4be19c1340037b3ba46a36e3a5
Jacob DeBerry <JMDeBerry99@gmail.com>
2021-06-06 20:54:49 -0400

    Adds scanner capabilities

---
+++ src/main/java/oop/exercises/BloodAlcoholCalculator.java
@@ -0,0 +1,15 @@
+/*
+ *  UCF COP3330 Summer 2021 Assignment 1 Solution
+ *  Copyright 2021 Jacob DeBerry
+ */
+package oop.exercises;
+import java.util.Scanner;
+
+public class BloodAlcoholCalculator
+{
+    public static void main( String[] args )
+    {
+        Scanner in = new Scanner(System.in);
+
+    }
+}
--- src/test/java/oop/exercises/BloodAlcoholCalculatorTest.java
+++ src/test/java/oop/exercises/BloodAlcoholCalculatorTest.java
@@ -5,9 +5,9 @@ import static org.junit.Assert.assertTrue;
 import org.junit.Test;
 
 /**
- * Unit test for simple App.
+ * Unit test for simple BloodAlcoholCalculator.
  */
-public class AppTest 
+public class BloodAlcoholCalculatorTest
 {
     /**
      * Rigorous Test :-)
